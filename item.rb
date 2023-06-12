46d779371d9ec9b3bc5be1fc48da6a724de2c20d
Salomon Tshobohwa <stshobohwa@gmail.com>
2023-06-12 18:47:20 +0200

    Create Item class

---
+++ item.rb
@@ -0,0 +1,24 @@
+class Item
+  def initialize(id, publish_date, archived)
+    @id = id
+    @source = source
+    @publish_date = publish_date
+    @archived = archived
+  end
+
+  def genre(genre)
+    @genre = genre
+  end
+
+  def author(author)
+    @author = author
+  end
+
+  def source(source)
+    @source = source
+  end
+
+  def label(label)
+    @label = label
+  end
+end
