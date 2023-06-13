ca611f0f2fac472b32a95b95db839e618329fd59
cherelemma <chere.lemma@aastu.edu.et>
2023-06-13 21:34:36 +0300

    add method for writting musicalbums to file

--- modules/preserve_music_album.rb
+++ modules/preserve_music_album.rb
@@ -1,5 +1,17 @@
 require 'json'
 
 module PreserveMusicAlbums
+  def write_music_album(musicalbums)
+    file = File.open('./data/music_album.json', 'w+')
+
+    music_album_hash = {}
+    musicalbums.each_with_index do |music_album, index|
+      music_album_hash[(index + 1).to_s] = { 'id' => music_album.id, 'title' => music_album.title,
+                                             'on_spotify' => music_album.on_spotify,
+                                             'publish_date' => music_album.publish_date,
+                                             'archived' => music_album.archived }
+    end
+    file.write(JSON.pretty_generate(music_album_hash))
+  end
 
 end
