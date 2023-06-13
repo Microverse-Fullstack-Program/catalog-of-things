eef0bd2e073fc006f6c50be91764c8ec741dde42
Salomon Tshobohwa <stshobohwa@gmail.com>
2023-06-13 10:21:46 +0200

    Use random integer id

--- item.rb
+++ item.rb
@@ -1,6 +1,6 @@
 class Item
-  def initialize(id, publish_date, archived)
-    @id = id
+  def initialize(publish_date, archived)
+    @id = Random.rand(1..1000)
     @publish_date = publish_date
     @archived = archived
   end
