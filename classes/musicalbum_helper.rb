aaacbfda93e2dc8deb95b22df3c7d241b1ce3fb7
cherelemma <chere.lemma@aastu.edu.et>
2023-06-13 20:32:38 +0300

    debug and fix path refernce error

--- classes/musicalbum_helper.rb
+++ classes/musicalbum_helper.rb
@@ -1,11 +1,11 @@
 require_relative 'music_album'
-require_relative 'modules/add_music_album'
-require_relative 'modules/list_music_albums'
-require_relative 'modules/preserve_music_album'
+require_relative '../modules/add_music_album'
+require_relative '../modules/list_music_albums'
+require_relative '../modules/preserve_music_album'
 require_relative 'genre'
-require_relative 'modules/add_genre'
-require_relative 'modules/list_genres'
-require_relative 'modules/preserve_genre'
+require_relative '../modules/add_genre'
+require_relative '../modules/list_genres'
+require_relative '../modules/preserve_genre'
 
 class MusicAlbumHelper
   include AddMusicAlbum
