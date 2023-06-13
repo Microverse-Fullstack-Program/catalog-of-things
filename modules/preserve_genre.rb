ce72599af8f015edccc06ddd88cccfa01e0df269
cherelemma <chere.lemma@aastu.edu.et>
2023-06-13 21:37:26 +0300

    write genre to file

--- modules/preserve_genre.rb
+++ modules/preserve_genre.rb
@@ -1,5 +1,14 @@
 require 'json'
 
 module PreserveGenres
+  def write_genre(genres)
+    file = File.open('./data/genre.json', 'w+')
+
+    genre_hash = {}
+    genres.each_with_index do |genre, index|
+      genre_hash[(index + 1).to_s] = { 'id' => genre.id, 'name' => genre.name, 'items' => genre.items }
+    end
+    file.write(JSON.pretty_generate(genre_hash))
+  end
 
 end
