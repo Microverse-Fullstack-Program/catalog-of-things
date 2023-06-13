9a88207e6ced47833428feb9ee16d1793b16f945
cherelemma <chere.lemma@aastu.edu.et>
2023-06-13 21:37:45 +0300

    read genre from file

--- modules/preserve_genre.rb
+++ modules/preserve_genre.rb
@@ -11,4 +11,15 @@ module PreserveGenres
     file.write(JSON.pretty_generate(genre_hash))
   end
 
+  def read_genre(genres)
+    file = File.open('./data/genre.json', 'r') if File.exist?('./data/genre.json')
+    return false if file.nil?
+
+    return unless file.size.positive?
+
+    genres_record = JSON.parse(file.read)
+    genres_record.each do |_key, genre|
+      genres << Genre.new(genre['name'])
+    end
+  end
 end
