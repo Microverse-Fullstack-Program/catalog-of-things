dc54043b6c9d9c341712fe91e54672981fd50a70
cherelemma <chere.lemma@aastu.edu.et>
2023-06-13 21:35:22 +0300

    add method to read musicalbum from file

--- modules/preserve_music_album.rb
+++ modules/preserve_music_album.rb
@@ -14,4 +14,15 @@ module PreserveMusicAlbums
     file.write(JSON.pretty_generate(music_album_hash))
   end
 
+  def read_music_album(musicalbums)
+    file = File.open('./data/music_album.json', 'r') if File.exist?('./data/music_album.json')
+    return false if file.nil?
+
+    return unless file.size.positive?
+
+    musicalbums_record = JSON.parse(file.read)
+    musicalbums_record.each do |_key, music_album|
+      musicalbums << MusicAlbum.new(music_album['title'], music_album['on_spotify'], music_album['publish_date'])
+    end
+  end
 end
