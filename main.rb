8de25476820c53d50663ae4fb2d50673ee33cec3
Ismail Ali <ismailchangezi801@gmail.com>
2023-06-14 21:16:50 +0500

    Update Main.rb to save and load data

--- main.rb
+++ main.rb
@@ -1,9 +1,11 @@
 require_relative './classes/musicalbum_helper'
 require_relative './classes/game_author_helper'
+require_relative './classes/booklabel_helper'
 class App
   def initialize
     @musicalbum_helper = MusicAlbumHelper.new
     @game_author_helper = GameAuthorHelper.new
+    @booklabel_helper = BookLabelHelper.new
   end
 
   def main_menu
@@ -31,7 +33,9 @@ class App
   def call_action(option)
     case option
     when 1
-      # To do: call Book catalog menu here
+      @booklabel_helper.load
+      @booklabel_helper.books_and_labels_menu
+      @booklabel_helper.save
     when 2
       @musicalbum_helper.music_album_menu
     when 3
@@ -43,5 +47,9 @@ class App
   end
 end
 
+
+
 app = App.new
+
 app.main_menu
+
