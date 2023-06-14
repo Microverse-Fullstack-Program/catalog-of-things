1fa1892cf12b35855edde7f926000ebc2f05d128
cherelemma <chere.lemma@aastu.edu.et>
2023-06-14 10:26:10 +0300

    test musicalbum properties return correct value after the abject created

--- spec/musicalbum_spec.rb
+++ spec/musicalbum_spec.rb
@@ -10,4 +10,19 @@ describe MusicAlbum do
       expect(@music_album).to be_instance_of MusicAlbum
     end
   end
+
+  context 'when music album object created' do
+    it 'Should return the title to be "Woy Fikir"' do
+      expect(@music_album.title).to eq 'Woy Fikir'
+    end
+
+    it 'Should return the publish date to be 1994/07/13' do
+      expect(@music_album.publish_date).to eq '1994/07/13'
+    end
+
+    it 'Should return the on_spotify to be true' do
+      expect(@music_album.on_spotify).to eq 'yes'
+    end
+  end
+
 end
