16285b5863edf7df82fbaee8d0c2559717350627
cherelemma <chere.lemma@aastu.edu.et>
2023-06-14 10:29:13 +0300

    update the Item class constructor

--- classes/item.rb
+++ classes/item.rb
@@ -1,29 +1,24 @@
+require 'date'
+
 class Item
-  def initialize(publish_date, archived)
-    @id = Random.rand(1..1000)
+  attr_accessor :publish_date, :genre, :author, :source, :label
+  attr_reader :id, :archived
+
+  def initialize(publish_date = nil)
+    @id = Random.rand(1000)
     @publish_date = publish_date
-    @archived = archived
-  end
-
-  def genre(genre)
-    @genre = genre
-  end
-
-  def author(author)
-    @author = author
-  end
-
-  def source(source)
-    @source = source
-  end
-
-  def label(label)
-    @label = label
+    @archived = can_be_archived?
+    @genre = nil
+    @author = nil
+    @source = nil
+    @label = nil
   end
 
   def can_be_archived?
-    @publish_date > 10
+    true if Date.today.year - Date.parse(@publish_date.to_s).year > 10
   end
+
+  private :can_be_archived?
 
   def move_to_archive
     return unless can_be_archived?
