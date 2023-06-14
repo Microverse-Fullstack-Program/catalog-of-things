f1d395df922b46e8332c393f70ed0426eb0c9560
Salomon Tshobohwa <stshobohwa@gmail.com>
2023-06-14 11:00:52 +0200

    Create parse_to_date helper function

---
+++ modules/parse_to_date.rb
@@ -0,0 +1,11 @@
+module ParseToDate
+  require 'date'
+
+  def parse_to_date(date_string)
+    Date.strptime(date_string, '%d-%m-%Y')
+  rescue ArgumentError
+    puts 'Invalid date format input, make sure that you use the (DD-MM-YYYY) format:'
+    date_string = gets.chomp
+    parse_to_date(date_string)
+  end
+end
