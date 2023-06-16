1173260d40fab887c352a4231aa2bfb95d6d9664
cherelemma <chere.lemma@aastu.edu.et>
2023-06-16 20:56:01 +0300

    update all classes constructor

--- classes/book.rb
+++ classes/book.rb
@@ -3,8 +3,8 @@ require_relative 'item'
 class Book < Item
   attr_accessor :publisher, :cover_state
 
-  def initialize(publisher, cover_state, publish_date, id: Random.rand(1..1000), archived: false)
-    super(publish_date, id: id, archived: archived)
+  def initialize(publisher, cover_state, publish_date, id = nil, archived: false)
+    super(publish_date, id, archived: archived)
     @publisher = publisher
     @cover_state = cover_state
   end
